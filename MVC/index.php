108eb1efbed4c15723718c996094d4bb398c02e0
helgrind-cyber <helgrindxxx@gmail.com>
2020-06-05 12:19:18 +0700

    Create MVC Project

---
+++ MVC/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "Mo hinh mvc";
+?>
---
+++ OOP/lession-5-ontap/demo.php
@@ -0,0 +1,25 @@
+<?php
+
+class Human {
+    private $name = "Quan";
+
+    function getName() {
+        echo $this->name;
+    }
+}
+
+$quan = new Human();
+$quan->getName();
+
+/**
+ * TODO: Tim hieu
+ * ! Name space
+ * ! Chuan dat ten: PSR4
+ * ! Cai composer
+ * ! Model, Controller, View
+ * ! Life cycle
+ */
+/**
+ * ! REQUEST LIFECYCLE
+ * keyword 'the standard Lifecycle'
+ */
